d20b0ab9675f952b556cfeea4fdd80157a5ab093
GuillermoMuena <31358858+GuillermoMuena@users.noreply.github.com>
2018-04-01 15:35:56 -0300

    Update pruebaAtom.cpp

--- pruebaAtom.cpp
+++ pruebaAtom.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 
 int main(void){
-  for (int i = 0; i < 20; i++) {
-    switch (i%2) {
-      case 1: std::cout << "Hello" << '\n';
-              break;
-      case 0: std::cout << "World" << '\n';
-    }
-  }
+  std::cout << "Hello World" << '\n';
   return 0;
 }
