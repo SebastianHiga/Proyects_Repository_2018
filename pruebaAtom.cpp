a88cdc779379f36ea94719dc711130083e79e9aa
guillermo <guillermuena@est.frba.utn.edu.ar>
2018-04-01 13:08:59 -0300

    First commit

---
+++ pruebaAtom.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+  for (int i = 0; i < 20; i++) {
+    switch (i%2) {
+      case 1: std::cout << "Hello" << '\n';
+              break;
+      case 0: std::cout << "World" << '\n';
+    }
+  }
+  return 0;
+}
