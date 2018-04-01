a0eac126f270ba8ceb583af4fea062b046d433af
GuillermoMuena <31358858+GuillermoMuena@users.noreply.github.com>
2018-04-01 15:20:37 -0300

    Update pruebaAtom.cpp

--- pruebaAtom.cpp
+++ pruebaAtom.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(void){
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i < 49; i++) {
     switch (i%2) {
       case 1: std::cout << "Hello" << '\n';
               break;
